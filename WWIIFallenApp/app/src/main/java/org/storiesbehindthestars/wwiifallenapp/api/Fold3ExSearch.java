a929e62362d9bc66491fccbb7944fe773eb1f276
14edavis <48037357+14edavis@users.noreply.github.com>
2021-08-30 19:43:52 -0600

    Linked up to Fold3's new API

--- WWIIFallenApp/app/src/main/java/org/storiesbehindthestars/wwiifallenapp/api/Fold3ExSearch.java
+++ WWIIFallenApp/app/src/main/java/org/storiesbehindthestars/wwiifallenapp/api/Fold3ExSearch.java
@@ -1,5 +1,10 @@
 package org.storiesbehindthestars.wwiifallenapp.api;
 
+import android.os.Build;
+import android.util.JsonReader;
+
+import androidx.annotation.RequiresApi;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -7,6 +12,7 @@ import org.json.JSONObject;
 import org.storiesbehindthestars.wwiifallenapp.models.Story;
 
 import java.io.IOException;
+import java.io.Reader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -18,8 +24,7 @@ public class Fold3ExSearch {
     private JSONObject jsonResponse;
     private String baseSearch = "https://www.fold3.com/ex-search?resultTypes=memorials&keywords=www.storiesbehindthestars.org,";
     private String endSearch = "&apiKey=x2P6Q2Pf9jT7xREfcAyBRjJX";//"&apiKey=PLACEHOLDER"; //todo: insert API Key
-
-
+    private String storybaseSearch = "https://www.fold3.com/partner-memorial?id=";
 
     public Fold3ExSearch(String textForSearch) throws IOException, JSONException {
         String convertedTextForSearch = textForSearch.replace(" ", ",");
@@ -40,16 +45,17 @@ public class Fold3ExSearch {
 //        return json;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     public Story[] getStories() throws JSONException, IOException {
         //loop through the JSON response to find all stories, identify which ones are actually stories...
         ArrayList<JSONObject> jsonStoryArrayList = new ArrayList<>();
-        JSONArray hitList = (JSONArray) jsonResponse.get("hits");
+        JSONArray memorialList = (JSONArray) jsonResponse.get("hits");
 
-        for (int i = 0; i<hitList.length(); i++){
-            JSONObject hitUrlObj = (JSONObject) hitList.get(i);
+        for (int i = 0; i<memorialList.length(); i++){
+            JSONObject hitUrlObj = (JSONObject) memorialList.get(i);
             String hitUrl = (String) hitUrlObj.get("url");
             if (hitUrl.contains("/page/")){
-                jsonStoryArrayList.add((JSONObject) hitList.get(i));
+                jsonStoryArrayList.add((JSONObject) memorialList.get(i));
             }
         }
 
@@ -68,13 +74,8 @@ public class Fold3ExSearch {
                 }
             }
 
-
             //WEBPAGE
             URL url = new URL((String) jsonStoryArrayList.get(i).get("url"));
-
-            //STORY
-            String story = readStoryFromWebpage(url);
-
 
             //TODO: This currently just scrapes the webpage to find the picture. Future updates should
             //      integrate actually with Fold3's database to find the proper images
@@ -84,6 +85,21 @@ public class Fold3ExSearch {
                     +pageID
                     +"?width=100&height=100"
                     +endSearch;
+            //STORY
+//                        String story = readStoryFromWebpage(url);
+            String story = "null";
+            try{
+                story = getStory(pageID);}
+            catch(IOException e){
+                story = "Unable to connect to Fold3.";
+            }
+            catch(JSONException e){
+                story = "JSON Exception Occurred";
+            }
+            catch(Exception e){
+                story = "Something went wrong with retrieving this story.";
+            }
+
 
             //TODO - connect to custom background. This is just a default.
             //The image loading was failing because fold3 updated their background picture label
@@ -103,7 +119,24 @@ public class Fold3ExSearch {
         return stories;
     }
 
-    //TODO: Update readStoryFromWebpage so that it connects directly to the database and is not dependent on the website HTML
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public String getStory(String pageID) throws JSONException, IOException {
+        URL url = new URL(storybaseSearch + pageID);
+        Scanner sc = new Scanner(url.openStream());
+        StringBuffer sb = new StringBuffer();
+        while(sc.hasNext()) {
+            sb.append(sc.next());
+        }
+        String result = sb.toString(); //string of JSON results
+        JSONObject jsonStoryResponse = new JSONObject(result); //create object
+        JSONArray storiesJSON = new JSONArray(jsonStoryResponse.get("stories")); //get the stories
+        JSONObject storyJSON = new JSONObject((String) storiesJSON.get(0)); //gets the first story
+        String storyText = (String) storyJSON.get("story"); //get the story's text
+        storyText = parseOutHTML(storyText);
+        storyText = translateAscii(storyText);
+        return result;
+    }
+
     public String readStoryFromWebpage(URL url) throws IOException {
         //Instantiating the URL class
 //        URL url = new URL("http://www.something.com/");
@@ -119,50 +152,38 @@ public class Fold3ExSearch {
         String result = sb.toString();
         System.out.println(result);
         //Removing the HTML tags
-
 //        final String startOfStoryText = "{\"metadata\":{\"id\":{\"contentType\":\"STORY\"";
-
         if (result.indexOf("StoryBlock-text\">") == -1){
 //        if (result.indexOf(startOfStoryText) == -1){
             return "";
         }
-
         result = result.substring(
                 result.indexOf("StoryBlock-text\">")+17
 
         );
         result = result.substring(0, result.indexOf("</div>"));
-
 //        result = result.replaceAll("<[^>]*>", "");
-
         System.out.println("Contents of the web page: "+result);
 
         result = parseOutHTML(result);
         result = translateAscii(result);
-
         return result;
     }
 
 
     public String parseOutHTML(String stringWithHTML){
-
         StringBuilder sb = new StringBuilder();
-
         while (stringWithHTML.contains("<") ){
             //search for sections
             int startTag = stringWithHTML.indexOf("<");
             int endTag = stringWithHTML.indexOf(">")+1;
-
             String htmlTag = stringWithHTML.substring(startTag, endTag);
             if (htmlTag.equals("</p>")){
                 sb.append("\n\n");
             }
-
             //remove those sections from orginal
             stringWithHTML = stringWithHTML.substring(endTag);
-
             int startNextTag = stringWithHTML.indexOf("<");
-
             if (startNextTag-1 > 0) {
                 sb.append(
                         stringWithHTML.substring(
@@ -171,29 +192,18 @@ public class Fold3ExSearch {
                         )
                 );
             }
-
-
-
         }
-
         return sb.toString();
     }
 
 
     String translateAscii(String storyText){
-
         for(int i = 21; i < 30; i++){
             String ascii = "&#x"+i+";"; //&#x21 = ascii 33
             String replacement = Character.toString( (char) (i+(33-21)));
-//            System.out.println(ascii + " " + replacement);
-
             storyText = storyText.replace(ascii, replacement);
-//            System.out.println(storyText);
         }
-
         return storyText;
-
-//        return storyText.replace("&#x27;", "'");
     }
 
 }
